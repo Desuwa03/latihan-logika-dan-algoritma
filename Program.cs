49145abafb93c62a686cab98d7e4d34c0a48270b
Desuwa03 <138005290+Desuwa03@users.noreply.github.com>
2025-11-15 16:32:59 +0700

    Menambahkan kode C#, logika Gerbang Penjaga

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class Exercise
+    {
+        static void Main()
+        {
+            Console.WriteLine("Masukkan 'true' atau 'false'");
+            string inputPedang = Console.ReadLine();
+            string inputArmor = Console.ReadLine();
+            string inputKunci = Console.ReadLine();
+
+            string gerbangTerbuka = "Gerbang Terbuka";
+            string gerbangMenyerang = "Gerbang Menyerang Player!";
+            string gerbangMenghilang = "Tidak ditemukan adanya Gerbang!";
+
+            bool isInputPedang = Convert.ToBoolean(inputPedang);
+            bool isInputArmor = Convert.ToBoolean(inputArmor);
+            bool isInputKunci = Convert.ToBoolean(inputKunci);
+
+            if ((isInputPedang && isInputArmor) || isInputKunci)
+            {
+                Console.WriteLine(gerbangTerbuka);
+            }
+            else if (isInputPedang || isInputArmor)
+            {
+                Console.WriteLine(gerbangMenyerang);
+            }
+            else {
+                Console.WriteLine(gerbangMenghilang);
+            }
+        }
+    }
+}
